1e4fe4a5203043b0d2f414d9e1a002029ace5b68
AVIRALUTTAM <aviraluttam@gmail.com>
2026-02-18 15:03:09 +0530

    Create App.jsx for React portfolio component

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to my Portfolio</h1>
+            <p>This is a showcase of my work and projects.</p>
+        </div>
+    );
+};
+
+export default App;
