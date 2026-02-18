8eb7e5d6b630839ab25dc155c1869f2a7f22e0fe
AVIRALUTTAM <aviraluttam@gmail.com>
2026-02-18 15:03:05 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * Tailwind CSS configuration file.
+ * @type { import('tailwindcss').Config }
+ */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
